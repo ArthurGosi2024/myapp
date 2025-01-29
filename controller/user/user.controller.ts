6cb40ad745185e6eed3fbc0abb32f6055f50ac1d
ArthurGosi2024 <arthurgosi.oficial@gmail.com>
2025-01-29 13:15:05 -0300

    feat: adding a new feature to send email to the user, with the confirmation token

--- controller/user/user.controller.ts
+++ controller/user/user.controller.ts
@@ -1,26 +1,32 @@
 import {Request, Response} from 'express'
-import {UserService} from "../service/user/user.service";
-import {IUser, User} from "../models/user/user.models";
-import {Route} from "../decorators/router.decorators";
-import {IFindUser} from "../interfaces/findUser.interfaces";
-import {STATUSCODE} from "../enum/statusCode/statusCode.enum";
-import {CryptoHelper} from "../helpers/crypto.helper";
-import {DAYS_ACCOUNT_BLOCKED, MAXIMUM_LOGIN_ATTEMPTS} from "../constants/user.constants";
-import {validateDto} from "../middleware/validateDTO";
-import {UserDto} from "../dtos/user.dto";
+import {UserService} from "../../service/user/user.service";
+import {IUser, User} from "../../models/user/user.models";
+import {Route} from "../../decorators/router.decorators";
+import {IFindUser} from "../../interfaces/findUser.interfaces";
+import {STATUSCODE} from "../../enum/statusCode/statusCode.enum";
+import {CryptoHelper} from "../../helpers/crypto.helper";
+import {DAYS_ACCOUNT_BLOCKED, MAXIMUM_LOGIN_ATTEMPTS, TIME_EXPIRATION_CONFIRM} from "../../constants/user.constants";
+import {validateDto} from "../../middleware/validateDTO";
+import {UserDto} from "../../dtos/user.dto";
+import {EmailService} from "../../service/email/email.service";
+import {tokenGenerator} from "../../helpers/token.helper";
+import {ISecurity} from "../../interfaces/security.interfaces";
 
 export class UserController {
 
-    private service: UserService;
+    private userService: UserService;
+    private emailService: EmailService;
 
     constructor() {
-        this.service = new UserService();
+        this.userService = new UserService();
+        this.emailService = new EmailService();
+
     }
 
     @Route("delete", "/deleteUser")
     async delete(request: Request, response: Response) {
 
-        const result = await this.service.delete(request.body as IUser)
+        const result = await this.userService.delete(request.body as IUser)
 
         if (result) {
             return response.status(201).send({
@@ -30,27 +36,68 @@ export class UserController {
         return response.status(404).send({})
     }
 
-    @Route("post", "/insert",validateDto(UserDto))
+    @Route("post", "/insert", validateDto(UserDto))
     async register(request: Request, response: Response) {
 
-        const user :  IUser = request.body
-        const result = await this.service.insert(user)
+        const user: IUser = request.body
+        const hasUserExist = await this.userService.insert(user)
 
-        if (!result) {
+        if (!hasUserExist) {
             return response.status(STATUSCODE.CONFLICT).send({
                 message: 'Usuário ja existente.'
             })
         }
-        return response.status(STATUSCODE.CREATED).send({
-            message: 'Usuário criado com sucesso.'
+
+        const findUser = await this.userService.findBy(user)
+
+        const token = tokenGenerator(32, '-')
+
+        const sendEmail = await this.emailService.sendEmail({
+            user,
+            email: {
+                subject: 'Confirmação de Identificação',
+                html: `
+                    <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+                        <h1 style="color: #4caf50;">Confirmação de Identificação</h1>
+                        <p>Olá,${user.name}</p>
+                        <p>Recebemos uma solicitação para confirmar sua identificação em nosso sistema. Para concluir o processo, use o código abaixo:</p>
+                        <h2 style="text-align: center; color: #4caf50;">${token}</h2>
+                        <p>Atenção o tempo maiximo para uso do token e de 15 minutos. Caso expire, tera que solicitar, outra confirmação de email.</p>
+                        <p>Se você não solicitou essa ação, ignore este e-mail. Caso tenha dúvidas, entre em contato com nossa equipe de suporte.</p>
+                        <p>Atenciosamente,</p>
+                        <p><strong>Equipe Api-Login</strong></p>
+                    </div>
+            `,
+            },
+        });
+
+        if (sendEmail) {
+            if (findUser) {
+                const dateNow = new Date()
+                dateNow.setMinutes(TIME_EXPIRATION_CONFIRM)
+                this.userService.update({email: findUser.email}, {
+                    ...findUser,
+                    security: {
+                        ...findUser.security,
+                        confirmEmailToken: token,
+                        confirmEmailExpiration: BigInt(dateNow.getTime()),
+                    } as ISecurity
+                })
+            }
+            return response.status(STATUSCODE.CREATED).send({
+                message: "Um token foi enviado para o seu e-mail. Utilize-o para confirmá-lo em nosso site."
+            })
+        }
+
+        return response.status(STATUSCODE.BAD_GATEWAY).send({
+            message: "Ocorreu um erro temporário com nosso provedor de e-mail."
         })
     }
 
     @Route("get", "/findUser")
     async login(request: Request, response: Response) {
-
         const {email, password}: IFindUser = request.body
-        const findUser = await this.service.findBy({dateOfBirth: "", name: "", email, password})
+        const findUser = await this.userService.findBy({dateOfBirth: "", name: "", email, password})
 
         if (findUser) {
             const dateNow = new Date();
@@ -67,7 +114,7 @@ export class UserController {
                 const releaseDate = new Date(Number(findUser.security.lockUntil))
 
                 if (dateNow.getTime() >= releaseDate.getTime()) {
-                    this.service.update({email: findUser.email}, {
+                    this.userService.update({email: findUser.email}, {
                         ...findUser,
                         security: {
                             ...findUser.security,
@@ -76,8 +123,6 @@ export class UserController {
                             lockUntil: 0n
                         }
                     })
-
-
                     return response.status(STATUSCODE.OK).send({
                         message: 'Acesso liberado para novas tentativas.'
                     })
@@ -97,7 +142,7 @@ export class UserController {
             if (passwordUser !== password) {
                 if (MAXIMUM_LOGIN_ATTEMPTS - findUser.security.failedLoginAttempts === 0) {
                     dateNow.setDate(dateNow.getDate() + DAYS_ACCOUNT_BLOCKED)
-                    this.service.update({email: findUser.email}, {
+                    this.userService.update({email: findUser.email}, {
                         ...findUser,
                         security: {
                             ...findUser.security,
@@ -111,7 +156,7 @@ export class UserController {
                     })
                 }
 
-                this.service.update({email: findUser.email}, {
+                this.userService.update({email: findUser.email}, {
                     ...findUser,
                     security: {
                         ...findUser.security,
