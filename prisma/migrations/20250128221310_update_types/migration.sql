0af1fc0a1942084e62795696a9954d5eb5afc2af
ArthurGosi2024 <arthurgosi.oficial@gmail.com>
2025-01-29 13:19:36 -0300

    add:adding new prism migration

---
+++ prisma/migrations/20250128221310_update_types/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `confirmEmailExpiration` on the `Security` table. The data in that column could be lost. The data in that column will be cast from `String` to `BigInt`.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Security" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
+    "failedLoginAttempts" INTEGER NOT NULL,
+    "isAccountLocked" BOOLEAN NOT NULL,
+    "lockUntil" BIGINT NOT NULL,
+    "passwordResetExpiration" TEXT NOT NULL,
+    "passwordResetToken" TEXT,
+    "confirmEmailAccount" BOOLEAN NOT NULL,
+    "confirmEmailToken" TEXT NOT NULL,
+    "confirmEmailExpiration" BIGINT NOT NULL,
+    CONSTRAINT "Security_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("userId") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Security" ("confirmEmailAccount", "confirmEmailExpiration", "confirmEmailToken", "failedLoginAttempts", "id", "isAccountLocked", "lockUntil", "passwordResetExpiration", "passwordResetToken", "userId") SELECT "confirmEmailAccount", "confirmEmailExpiration", "confirmEmailToken", "failedLoginAttempts", "id", "isAccountLocked", "lockUntil", "passwordResetExpiration", "passwordResetToken", "userId" FROM "Security";
+DROP TABLE "Security";
+ALTER TABLE "new_Security" RENAME TO "Security";
+CREATE UNIQUE INDEX "Security_id_key" ON "Security"("id");
+CREATE UNIQUE INDEX "Security_userId_key" ON "Security"("userId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
