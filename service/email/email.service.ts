c082856fb541a080e55a5148877b898981787454
ArthurGosi2024 <arthurgosi.oficial@gmail.com>
2025-01-30 22:21:40 -0300

    refactor

--- service/email/email.service.ts
+++ service/email/email.service.ts
@@ -1,65 +1,54 @@
-import {IEmailSendProps, IEmailServiceContract} from "../../interfaces/email.interfaces";
+import {
+	IEmailSendProps,
+	IEmailServiceContract,
+} from "../../interfaces/email.interfaces";
 
-import {createTransport, Transporter} from 'nodemailer'
+import { createTransport, Transporter } from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
+import { UserService } from "../user/user.service";
+import { User } from "../../models/user/user.models";
 
 class Nodemaile {
+	private readonly nodemailer: Transporter<
+		SMTPTransport.SentMessageInfo,
+		SMTPTransport.Options
+	> | null = null;
 
-    private readonly nodemailer: Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options> | null = null;
+	constructor() {
+		if (!this.nodemailer) {
+			this.nodemailer = createTransport({
+				service: "gmail",
+				auth: {
+					user: process.env.USER,
+					pass: process.env.PASS,
+				},
+			});
+		}
+	}
 
-    constructor() {
-        if (!this.nodemailer) {
-            this.nodemailer = createTransport({
-                service: "gmail",
-                auth: {
-                    user: process.env.USER,
-                    pass: process.env.PASS,
-                }
-            })
-        }
-    }
-
-
-    public getInstanceNodemailer() {
-        return this.nodemailer
-    }
+	public getInstanceNodemailer() {
+		return this.nodemailer;
+	}
 }
 
 export class EmailService implements IEmailServiceContract<any> {
+	private nodemailer: Nodemaile = new Nodemaile();
 
-    private nodemailer: Nodemaile = new Nodemaile();
+	constructor() {}
 
-    constructor() {
+	async sendEmail(data: IEmailSendProps): Promise<boolean> {
+		const { accepted } = await this.nodemailer
+			.getInstanceNodemailer()
+			.sendMail({
+				from: process.env.EMAIL_PROVIDER,
+				to: {
+					address: data.user.email,
+					name: data.user.name,
+				},
+				subject: data.email.subject,
+				html: data.email.html,
+			});
 
-    }
-
-    confirmEmail(data: any): void {
-
-    }
-
-    forgotPassword(data: any): void {
-    }
-
-    recoverPassword(data: any): void {
-    }
-
-    resendEmailConfirmation(data: any): void {
-    }
-
-
-    async sendEmail(data: IEmailSendProps): Promise<boolean> {
-
-        const {accepted} = await this.nodemailer.getInstanceNodemailer().sendMail({
-            from: process.env.EMAIL_PROVIDER, // sender address
-            to: {
-                address: data.user.email,
-                name: data.user.name,
-            }, // list of receivers
-            subject: data.email.subject, // Subject line
-            html: data.email.html, // html body
-        })
-
-        return accepted.every(email => email === data.user.email)
-    }
-
+		return accepted.every((email) => email === data.user.email);
+	}
 }
--- service/user/user.service.ts
+++ service/user/user.service.ts
@@ -1,54 +1,49 @@
-import {IContractServiceCrudGenerics} from "../../interfaces/contractCrudGenerics.interfaces";
-import {IUser, User} from "../../models/user/user.models";
-import {UserRepository} from "../../repository/user/user.repository";
-import {CryptoHelper} from "../../helpers/crypto.helper";
-import {IFindUser} from "../../interfaces/findUser.interfaces";
-import {IUpdateUser} from "../../interfaces/updateUser.interfaces";
-import {IDeleteUser} from "../../interfaces/deleteUser.interfaces";
-
-
+import { IContractServiceCrudGenerics } from "../../interfaces/contractCrudGenerics.interfaces";
+import { IUser } from "../../models/user/user.models";
+import { UserRepository } from "../../repository/user/user.repository";
+import { CryptoHelper } from "../../helpers/crypto.helper";
+import { IFindUser } from "../../interfaces/findUser.interfaces";
+import { IDeleteUser } from "../../interfaces/deleteUser.interfaces";
 
 export class UserService implements IContractServiceCrudGenerics<IUser> {
+	private readonly userRepository: UserRepository | null = null;
 
-    private readonly userRepository: UserRepository | null = null;
+	constructor() {
+		this.userRepository = new UserRepository();
+	}
 
-    constructor() {
-        this.userRepository = new UserRepository()
-    }
+	async delete(data: IDeleteUser): Promise<Boolean> {
+		const findUser = await this.userRepository.findBy({ email: data.email });
 
+		if (findUser) {
+			await this.userRepository.delete({ email: data.email });
+			return true;
+		}
+		return false;
+	}
 
-    async delete(data: IDeleteUser): Promise<Boolean> {
-        const findUser = await this.userRepository.findBy({email: data.email});
+	async insert(data: IUser): Promise<Boolean> {
+		const existsUser = await this.findBy({ email: data.email });
 
-        if (findUser) {
-            await this.userRepository.delete({email: data.email})
-            return true
-        }
-        return false
-    }
+		if (existsUser) {
+			return false;
+		}
 
-    async insert(data: IUser): Promise<Boolean> {
+		data.password = CryptoHelper.encrypt(data.password);
+		await this.userRepository.insert(data);
+		return true;
+	}
 
-        const existsUser = await this.findBy({email : data.email});
+	update(data: Partial<IUser>, newData: Partial<IUser>): void {
+		this.userRepository.update(
+			{
+				email: data.email,
+			},
+			newData
+		);
+	}
 
-        if (existsUser) {
-            return false;
-        }
-
-        data.password = CryptoHelper.encrypt(data.password)
-        await this.userRepository.insert(data);
-        return true
-    }
-
-    update(data: Partial<IUser>, newData: Partial<IUser>): void {
-        this.userRepository.update({
-            email: data.email
-        }, newData);
-    }
-
-    async findBy(data: Partial<IFindUser>): Promise<IFindUser> {
-        const findUser = await this.userRepository.findBy({email: data.email})
-        return findUser;
-    }
-
+	async findBy(data: Partial<IFindUser>): Promise<IFindUser> {
+		return await this.userRepository.findBy({ email: data.email });
+	}
 }
