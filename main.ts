309a4ebc8312ce816a70704c0d5f2c31c1b80d75
ArthurGosi2024 <arthurgosi.oficial@gmail.com>
2025-01-30 23:44:20 -0300

    chore: added cors

--- main.ts
+++ main.ts
@@ -1,54 +1,65 @@
-import Express, {NextFunction, Request, Response} from 'express';
-import {UserController} from "./controller/user/user.controller";
-import {routes} from "./decorators/router.decorators";
+import Express from "express";
+import { UserController } from "./controller/user/user.controller";
+import { routes } from "./decorators/router.decorators";
 import helmet from "helmet";
-import session from 'express-session'
-import {EmailController} from "./controller/email/email.controller";
+import session from "express-session";
+import { EmailController } from "./controller/email/email.controller";
+
+import cors from "cors";
 
 const app = Express();
 
-app.use(Express.json())
-app.use(Express.urlencoded({extended: true}))
-app.use(helmet())
+const corsOptions = {
+	origin: process.env.DOMAIN_FRONTEND,
+	methods: ["GET", "POST", "DELETE"],
+	allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use(session({
-        secret: process.env.SESSION_SECRET,
-        resave: true,         // Não re-salvar a sessão se não houve modificações
-        saveUninitialized: true, // Salvar uma sessão nova, mesmo que não tenha dados
-        cookie: {
-            httpOnly: true,       // Impede o acesso ao cookie via JavaScript
-            secure: false,        // Definir como true se estiver usando HTTPS
-            maxAge: 60000,        // Duração do cookie (aqui está configurado para 1 minuto)
-        },
-    })
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: true,
+		saveUninitialized: true,
+		cookie: {
+			httpOnly: true,
+			secure: false,
+			maxAge: 60000,
+		},
+	})
 );
 
 const userController = new UserController();
 const emailController = new EmailController();
 
-
-const controllers = {userController, emailController};
+const controllers = { userController, emailController };
 
 routes.forEach((route) => {
-    const boundHandler = Object.values(controllers)
-        .find(ctrl => Object.getPrototypeOf(ctrl).hasOwnProperty(route.handler.name))
-        ? route.handler.bind(Object.values(controllers).find(ctrl => Object.getPrototypeOf(ctrl).hasOwnProperty(route.handler.name)))
-        : route.handler;
+	const boundHandler = Object.values(controllers).find((ctrl) =>
+		Object.getPrototypeOf(ctrl).hasOwnProperty(route.handler.name)
+	)
+		? route.handler.bind(
+				Object.values(controllers).find((ctrl) =>
+					Object.getPrototypeOf(ctrl).hasOwnProperty(route.handler.name)
+				)
+		  )
+		: route.handler;
 
-    app[route.method](
-        route.path,
-        route.middleware || ((_, __, next) => next()),
-        boundHandler
-    );
+	app[route.method](
+		route.path,
+		route.middleware || ((_, __, next) => next()),
+		boundHandler
+	);
 });
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on process.e ${process.env.PORT}`)
-})
-
-process.once('SIGINT', () => {
-    console.log("Conexão com o bando de dados, encerrada com sucesso.")
-    process.exit();
+	console.log(`Example app listening on process.e ${process.env.PORT}`);
 });
 
-
+process.once("SIGINT", () => {
+	console.log("Conexão com o bando de dados, encerrada com sucesso.");
+	process.exit();
+});
