13910dff992da91570f26107fcacc10289a808ab
ArthurGosi2024 <arthurgosi.oficial@gmail.com>
2025-01-30 22:19:44 -0300

    feat: added new features for forgetting password and sending password recovery by email

--- controller/email/email.controller.ts
+++ controller/email/email.controller.ts
@@ -1,29 +1,251 @@
-import {EmailService} from "../../service/email/email.service";
-import {Route} from "../../decorators/router.decorators";
-import {Request, Response} from 'express'
-
+import { Route } from "../../decorators/router.decorators";
+import { Request, Response } from "express";
+import { UserService } from "../../service/user/user.service";
+import { STATUSCODE } from "../../enum/statusCode/statusCode.enum";
+import { EmailService } from "../../service/email/email.service";
+import { tokenGenerator } from "../../helpers/token.helper";
+import {TIME_EXPIRATION_CONFIRM_EMAIL, TIME_EXPIRATION_FORGOT_PASSWORD} from "../../constants/user.constants";
+import { ISecurity } from "../../interfaces/security.interfaces";
+import {RecoverDTO} from "../../dtos/user.dto";
+import {validateDto} from "../../middleware/validateDTO";
 
 export class EmailController {
+	private userService: UserService;
+	private emailService: EmailService;
 
-    private emailService: EmailService;
+	constructor() {
+		this.userService = new UserService();
+		this.emailService = new EmailService();
+	}
+
+	@Route("post", "/confirm-email")
+	async confirmEmail(request: Request, response: Response) {
+		const { email, token } = request.body as { token: string; email: string };
+		const findUser = await this.userService.findBy({ email });
+
+		if (!findUser) {
+			return response.status(STATUSCODE.NOT_FOUND).send({
+				message: "Usuário não encontrado.",
+			});
+		}
+
+		const timeNow = new Date().getTime();
+		const tokenOfConfirmation = findUser.security.confirmEmailToken;
+		const hasConfirmEmailAccount = findUser.security.confirmEmailAccount;
+
+		if (hasConfirmEmailAccount) {
+			return response.status(STATUSCODE.UNAUTHORIZED).send({
+				message: "Esta conta ja possue um email verificado.",
+			});
+		}
+
+		const timeOfConfirmationEmail = Number(
+			findUser.security.confirmEmailExpiration
+		);
+
+		if (timeNow > timeOfConfirmationEmail) {
+			return response.status(STATUSCODE.UNAUTHORIZED).send({
+				message: "Token expirado.",
+			});
+		}
+
+		if (tokenOfConfirmation !== token) {
+			return response.status(STATUSCODE.CONFLICT).send({
+				message: "Token incorreto.",
+			});
+		}
+
+		this.userService.update(
+			{ ...findUser },
+			{
+				...findUser,
+				security: {
+					...findUser.security,
+					confirmEmailToken: "",
+					confirmEmailExpiration: 0n,
+					confirmEmailAccount: true,
+				},
+			}
+		);
+
+		const hasAuthenticated = request.session["authenticated"];
+
+		if (!hasAuthenticated) {
+			request.session["authenticated"] = true;
+		}
+
+		return response.status(STATUSCODE.OK).send({
+			message: "Token verificado com sucesso.",
+		});
+	}
 
 
-    constructor() {
-        this.emailService = new EmailService();
-    }
+	@Route("post","/forgot-password")
+	async forgotPassword(request: Request, response: Response) {
+		const { email } = request.body as { email : string;   }
+
+		const user = await this.userService.findBy({ email });
+
+		if (!user) {
+			return response.status(STATUSCODE.UNAUTHORIZED).send({
+				message: 'Usuário não encontrado.'
+			})
+		}
+
+		const dateNow = new Date()
+		const currentTime = dateNow.getTime()
+		const tokenPasswordResend = user.security.passwordResetToken
+		const timeExpirationPassword = Number(user.security.passwordResetExpiration)
+
+		if (timeExpirationPassword > 0 && timeExpirationPassword < currentTime && tokenPasswordResend.length) {
+			return response.status(STATUSCODE.SERVICE_UNAVAILABLE).send({
+				message: "Um token ja foi enviado ao seu email, utilize-o para recuperar sua senha."
+			})
+		}
+
+		const token = tokenGenerator(10,'-')
+
+		const sendEmail = await this.emailService.sendEmail({
+			user,
+			email: {
+				subject: "Recuperação de Senha",
+				html: `
+            <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+                <h1 style="color: #4caf50;">Recuperação de Senha</h1>
+                <p>Olá, ${user.name}</p>
+                <p>Recebemos uma solicitação para redefinir sua senha. Para continuar, utilize o link abaixo:</p>
+                <h2 style="text-align: center ; font-weight: bold">
+                	${token}
+                </h2>
+                <p>Atenção, o tempo máximo para uso do link é de 15 minutos. Caso expire, será necessário solicitar uma nova recuperação de senha.</p>
+                <p>Se você não solicitou essa ação, ignore este e-mail. Caso tenha dúvidas, entre em contato com nossa equipe de suporte.</p>
+                <p>Atenciosamente,</p>
+                <p><strong>Equipe Api-Login</strong></p>
+            </div>
+        `,
+			},
+		});
+
+		if (sendEmail) {
+			dateNow.setMinutes(TIME_EXPIRATION_FORGOT_PASSWORD)
+			this.userService.update(user,{...user, security : {
+			...user.security,
+				passwordResetToken : token,
+				passwordResetExpiration: BigInt(dateNow.getTime())
+			}})
+			return response.status(STATUSCODE.CREATED).send({
+				message:
+					"Um token foi enviado para o seu e-mail. Utilize-o para recuperar a senha em nosso site.",
+			});
+		}
+
+		return response.status(STATUSCODE.BAD_GATEWAY).send({
+			message: "Ocorreu um erro temporário com nosso provedor de e-mail.",
+		});
+
+	}
+	@Route("post","/recover-password",validateDto(RecoverDTO))
+	 async recoverPassword(request: Request, response: Response) {
+		const {  token,email, password } = request.body as { email : string; password: string, token: string; }
+
+		 const user = await this.userService.findBy({ email });
+
+		 if (!user) {
+			 return response.status(STATUSCODE.UNAUTHORIZED).send({
+				 message: 'Usuário não encontrado.'
+			 })
+		 }
+
+		 const dateNow = new Date();
+		 const currentTime = dateNow.getTime();
+		 const currentTokenReset = user.security.passwordResetToken
+		 const currentTimeOfExpirationPasswordReset = Number(user.security.passwordResetExpiration);
+
+		 if (currentTime > currentTimeOfExpirationPasswordReset) {
+			 return response.status(STATUSCODE.CONFLICT).send({ message: "Token expirado." })
+		 }
 
 
-    @Route("post", "/confirm-email")
-    confirmEmail(request: Request, response: Response): void {
-    }
+		 if (token !== currentTokenReset) {
+			 return response.status(STATUSCODE.CONFLICT).send({
+				 message: 'Token incorreto.'
+			 })
+		 }
 
-    forgotPassword(data: any): void {
-    }
+		 this.userService.update(user,{
+			 ...user,
+			 password : password,
+			 security : {
+				 ...user.security,
+				 passwordResetExpiration : 0n,
+				 passwordResetToken : "",
+			 }
+		 })
 
-    recoverPassword(data: any): void {
-    }
 
-    resendEmailConfirmation(data: any): void {
-    }
+		return response.status(STATUSCODE.OK).send({
+			message: 'Senha alterada com sucesso.'
+		})
+	}
 
+	@Route("post", "/resend-email-confirmation")
+	async resendEmailConfirmation(request: Request, response: Response) {
+		const { email } = request.body as { email: string };
+
+		const user = await this.userService.findBy({ email });
+
+		if (user) {
+			const hasConfirmEmailAccount = user.security.confirmEmailAccount;
+			if (hasConfirmEmailAccount) {
+				return response.status(STATUSCODE.UNAUTHORIZED).send({
+					message: "Esta conta ja possue um email verificado.",
+				});
+			}
+
+			const token = tokenGenerator(32, "-");
+
+			const sendEmail = await this.emailService.sendEmail({
+				user,
+				email: {
+					subject: "Confirmação de Identificação",
+					html: `
+                    <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
+                        <h1 style="color: #4caf50;">Confirmação de Identificação</h1>
+                        <p>Olá,${user.name}</p>
+                        <p>Recebemos uma solicitação para confirmar sua identificação em nosso sistema. Para concluir o processo, use o código abaixo:</p>
+                        <h2 style="text-align: center; color: #4caf50;">${token}</h2>
+                        <p>Atenção o tempo maiximo para uso do token e de 15 minutos. Caso expire, tera que solicitar, outra confirmação de email.</p>
+                        <p>Se você não solicitou essa ação, ignore este e-mail. Caso tenha dúvidas, entre em contato com nossa equipe de suporte.</p>
+                        <p>Atenciosamente,</p>
+                        <p><strong>Equipe Api-Login</strong></p>
+                    </div>
+            `,
+				},
+			});
+
+			if (sendEmail) {
+				const dateNow = new Date();
+				dateNow.setMinutes(TIME_EXPIRATION_CONFIRM_EMAIL);
+				this.userService.update(
+					{ email: user.email },
+					{
+						...user,
+						security: {
+							...user.security,
+							confirmEmailToken: token,
+							confirmEmailExpiration: BigInt(dateNow.getTime()),
+						} as ISecurity,
+					}
+				);
+				return response.status(STATUSCODE.CREATED).send({
+					message:
+						"Um token foi enviado para o seu e-mail. Utilize-o para confirmá-lo em nosso site.",
+				});
+			}
+
+			return response.status(STATUSCODE.BAD_GATEWAY).send({
+				message: "Ocorreu um erro temporário com nosso provedor de e-mail.",
+			});
+		}
+	}
 }
--- controller/user/user.controller.ts
+++ controller/user/user.controller.ts
@@ -1,62 +1,62 @@
-import {Request, Response} from 'express'
-import {UserService} from "../../service/user/user.service";
-import {IUser, User} from "../../models/user/user.models";
-import {Route} from "../../decorators/router.decorators";
-import {IFindUser} from "../../interfaces/findUser.interfaces";
-import {STATUSCODE} from "../../enum/statusCode/statusCode.enum";
-import {CryptoHelper} from "../../helpers/crypto.helper";
-import {DAYS_ACCOUNT_BLOCKED, MAXIMUM_LOGIN_ATTEMPTS, TIME_EXPIRATION_CONFIRM} from "../../constants/user.constants";
-import {validateDto} from "../../middleware/validateDTO";
-import {UserDto} from "../../dtos/user.dto";
-import {EmailService} from "../../service/email/email.service";
-import {tokenGenerator} from "../../helpers/token.helper";
-import {ISecurity} from "../../interfaces/security.interfaces";
+import { Request, Response } from "express";
+import { UserService } from "../../service/user/user.service";
+import { IUser } from "../../models/user/user.models";
+import { Route } from "../../decorators/router.decorators";
+import { IFindUser } from "../../interfaces/findUser.interfaces";
+import { STATUSCODE } from "../../enum/statusCode/statusCode.enum";
+import { CryptoHelper } from "../../helpers/crypto.helper";
+import {
+	DAYS_ACCOUNT_BLOCKED,
+	MAXIMUM_LOGIN_ATTEMPTS,
+	TIME_EXPIRATION_CONFIRM_EMAIL,
+} from "../../constants/user.constants";
+import { validateDto } from "../../middleware/validateDTO";
+import { UserDto } from "../../dtos/user.dto";
+import { EmailService } from "../../service/email/email.service";
+import { tokenGenerator } from "../../helpers/token.helper";
+import { ISecurity } from "../../interfaces/security.interfaces";
 
 export class UserController {
+	private userService: UserService;
+	private emailService: EmailService;
 
-    private userService: UserService;
-    private emailService: EmailService;
+	constructor() {
+		this.userService = new UserService();
+		this.emailService = new EmailService();
+	}
 
-    constructor() {
-        this.userService = new UserService();
-        this.emailService = new EmailService();
+	@Route("delete", "/deleteUser")
+	async delete(request: Request, response: Response) {
+		const result = await this.userService.delete(request.body as IUser);
 
-    }
+		if (result) {
+			return response.status(201).send({
+				message: "User deleted successfully",
+			});
+		}
+		return response.status(404).send({});
+	}
 
-    @Route("delete", "/deleteUser")
-    async delete(request: Request, response: Response) {
+	@Route("post", "/insert", validateDto(UserDto))
+	async register(request: Request, response: Response) {
+		const user: IUser = request.body;
+		const hasUserExist = await this.userService.insert(user);
 
-        const result = await this.userService.delete(request.body as IUser)
+		if (!hasUserExist) {
+			return response.status(STATUSCODE.CONFLICT).send({
+				message: "Usuário ja existente.",
+			});
+		}
 
-        if (result) {
-            return response.status(201).send({
-                message: "User deleted successfully",
-            })
-        }
-        return response.status(404).send({})
-    }
+		const findUser = await this.userService.findBy(user);
 
-    @Route("post", "/insert", validateDto(UserDto))
-    async register(request: Request, response: Response) {
+		const token = tokenGenerator(32, "-");
 
-        const user: IUser = request.body
-        const hasUserExist = await this.userService.insert(user)
-
-        if (!hasUserExist) {
-            return response.status(STATUSCODE.CONFLICT).send({
-                message: 'Usuário ja existente.'
-            })
-        }
-
-        const findUser = await this.userService.findBy(user)
-
-        const token = tokenGenerator(32, '-')
-
-        const sendEmail = await this.emailService.sendEmail({
-            user,
-            email: {
-                subject: 'Confirmação de Identificação',
-                html: `
+		const sendEmail = await this.emailService.sendEmail({
+			user,
+			email: {
+				subject: "Confirmação de Identificação",
+				html: `
                     <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333;">
                         <h1 style="color: #4caf50;">Confirmação de Identificação</h1>
                         <p>Olá,${user.name}</p>
@@ -68,118 +68,150 @@ export class UserController {
                         <p><strong>Equipe Api-Login</strong></p>
                     </div>
             `,
-            },
-        });
+			},
+		});
 
-        if (sendEmail) {
-            if (findUser) {
-                const dateNow = new Date()
-                dateNow.setMinutes(TIME_EXPIRATION_CONFIRM)
-                this.userService.update({email: findUser.email}, {
-                    ...findUser,
-                    security: {
-                        ...findUser.security,
-                        confirmEmailToken: token,
-                        confirmEmailExpiration: BigInt(dateNow.getTime()),
-                    } as ISecurity
-                })
-            }
-            return response.status(STATUSCODE.CREATED).send({
-                message: "Um token foi enviado para o seu e-mail. Utilize-o para confirmá-lo em nosso site."
-            })
-        }
+		if (sendEmail) {
+			if (findUser) {
+				const dateNow = new Date();
+				dateNow.setMinutes(TIME_EXPIRATION_CONFIRM_EMAIL);
+				this.userService.update(
+					{ email: findUser.email },
+					{
+						...findUser,
+						security: {
+							...findUser.security,
+							confirmEmailToken: token,
+							confirmEmailExpiration: BigInt(dateNow.getTime()),
+						} as ISecurity,
+					}
+				);
+			}
+			return response.status(STATUSCODE.CREATED).send({
+				message:
+					"Um token foi enviado para o seu e-mail. Utilize-o para confirmá-lo em nosso site.",
+			});
+		}
 
-        return response.status(STATUSCODE.BAD_GATEWAY).send({
-            message: "Ocorreu um erro temporário com nosso provedor de e-mail."
-        })
-    }
+		return response.status(STATUSCODE.BAD_GATEWAY).send({
+			message: "Ocorreu um erro temporário com nosso provedor de e-mail.",
+		});
+	}
 
-    @Route("get", "/findUser")
-    async login(request: Request, response: Response) {
-        const {email, password}: IFindUser = request.body
-        const findUser = await this.userService.findBy({dateOfBirth: "", name: "", email, password})
+	@Route("get", "/findUser")
+	async login(request: Request, response: Response) {
+		const { email, password }: IFindUser = request.body;
+		const findUser = await this.userService.findBy({
+			dateOfBirth: "",
+			name: "",
+			email,
+			password,
+		});
 
-        if (findUser) {
-            const dateNow = new Date();
-            const isAccountLocked = findUser.security.isAccountLocked;
-            const confirmEmailAccount = findUser.security.confirmEmailAccount;
+		if (findUser) {
+			const dateNow = new Date();
+			const isAccountLocked = findUser.security.isAccountLocked;
+			const confirmEmailAccount = findUser.security.confirmEmailAccount;
 
-            if (!confirmEmailAccount) {
-                return response.status(STATUSCODE.NOT_FOUND).send({
-                    message: 'Não identificamos a confirmação de email, identifique seu email.'
-                })
-            }
+			if (!confirmEmailAccount) {
+				return response.status(STATUSCODE.NOT_FOUND).send({
+					message:
+						"Não identificamos a confirmação de email, identifique seu email.",
+				});
+			}
 
-            if (isAccountLocked) {
-                const releaseDate = new Date(Number(findUser.security.lockUntil))
+			if (isAccountLocked) {
+				const releaseDate = new Date(Number(findUser.security.lockUntil));
 
-                if (dateNow.getTime() >= releaseDate.getTime()) {
-                    this.userService.update({email: findUser.email}, {
-                        ...findUser,
-                        security: {
-                            ...findUser.security,
-                            failedLoginAttempts: 0,
-                            isAccountLocked: false,
-                            lockUntil: 0n
-                        }
-                    })
-                    return response.status(STATUSCODE.OK).send({
-                        message: 'Acesso liberado para novas tentativas.'
-                    })
-                }
+				if (dateNow.getTime() >= releaseDate.getTime()) {
+					this.userService.update(
+						{ email: findUser.email },
+						{
+							...findUser,
+							security: {
+								...findUser.security,
+								failedLoginAttempts: 0,
+								isAccountLocked: false,
+								lockUntil: 0n,
+							},
+						}
+					);
+					return response.status(STATUSCODE.OK).send({
+						message: "Acesso liberado para novas tentativas.",
+					});
+				}
 
-                return response.status(STATUSCODE.CONFLICT).send({
-                    message: `Tentativas bloqueadas. Tente novamente no dia ${releaseDate.toLocaleString('pt-BR', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: 'numeric',
-                    })}.`
-                })
-            }
+				return response.status(STATUSCODE.CONFLICT).send({
+					message: `Tentativas bloqueadas. Tente novamente no dia ${releaseDate.toLocaleString(
+						"pt-BR",
+						{
+							year: "numeric",
+							month: "long",
+							day: "numeric",
+						}
+					)}.`,
+				});
+			}
 
-            const passwordUser = CryptoHelper.decrypt(findUser.password);
+			const passwordUser = CryptoHelper.decrypt(findUser.password);
 
-            if (passwordUser !== password) {
-                if (MAXIMUM_LOGIN_ATTEMPTS - findUser.security.failedLoginAttempts === 0) {
-                    dateNow.setDate(dateNow.getDate() + DAYS_ACCOUNT_BLOCKED)
-                    this.userService.update({email: findUser.email}, {
-                        ...findUser,
-                        security: {
-                            ...findUser.security,
-                            failedLoginAttempts: findUser.security.failedLoginAttempts + 1,
-                            isAccountLocked: true,
-                            lockUntil: BigInt(dateNow.getTime())
-                        }
-                    })
-                    return response.status(STATUSCODE.BAD_REQUEST).send({
-                        message: 'Seu login foi temporariamente bloqueado.'
-                    })
-                }
+			if (passwordUser !== password) {
+				if (
+					MAXIMUM_LOGIN_ATTEMPTS - findUser.security.failedLoginAttempts ===
+					0
+				) {
+					dateNow.setDate(dateNow.getDate() + DAYS_ACCOUNT_BLOCKED);
+					this.userService.update(
+						{ email: findUser.email },
+						{
+							...findUser,
+							security: {
+								...findUser.security,
+								failedLoginAttempts: findUser.security.failedLoginAttempts + 1,
+								isAccountLocked: true,
+								lockUntil: BigInt(dateNow.getTime()),
+							},
+						}
+					);
+					return response.status(STATUSCODE.BAD_REQUEST).send({
+						message: "Seu login foi temporariamente bloqueado.",
+					});
+				}
 
-                this.userService.update({email: findUser.email}, {
-                    ...findUser,
-                    security: {
-                        ...findUser.security,
-                        failedLoginAttempts: findUser.security.failedLoginAttempts + 1
-                    }
-                })
+				this.userService.update(
+					{ email: findUser.email },
+					{
+						...findUser,
+						security: {
+							...findUser.security,
+							failedLoginAttempts: findUser.security.failedLoginAttempts + 1,
+						},
+					}
+				);
 
-                return response.status(STATUSCODE.CONFLICT).send({
-                    message: `Senha incorreta. Você ainda possue mais ${MAXIMUM_LOGIN_ATTEMPTS - findUser.security.failedLoginAttempts} tentativas.`
-                })
-            }
+				return response.status(STATUSCODE.CONFLICT).send({
+					message: `Senha incorreta. Você ainda possue mais ${
+						MAXIMUM_LOGIN_ATTEMPTS - findUser.security.failedLoginAttempts
+					} tentativas.`,
+				});
+			}
 
-            return response.status(STATUSCODE.FOUND).send({
-                data: {
-                    email: findUser.email,
-                    name: findUser.name,
-                    roles: findUser.roles,
-                } as IUser,
-                message: 'Acesso liberado.'
-            })
-        }
-        return response.status(STATUSCODE.NOT_FOUND).send({
-            message: 'Usuário não encontrado.'
-        })
-    }
+			const hasAuthenticated = request.session["authenticated"];
+
+			if (!hasAuthenticated) {
+				request.session["authenticated"] = true;
+			}
+			return response.status(STATUSCODE.FOUND).send({
+				data: {
+					email: findUser.email,
+					name: findUser.name,
+					roles: findUser.roles,
+				} as IUser,
+				message: "Acesso liberado.",
+			});
+		}
+		return response.status(STATUSCODE.NOT_FOUND).send({
+			message: "Usuário não encontrado.",
+		});
+	}
 }
